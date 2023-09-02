2a7a148b2f823fa11e02a1a4ccf23d1352b8c1ad
JiananYuan <yuanjiajiaqq@163.com>
2023-09-02 20:42:48 +0800

    feat

--- examples/exp2.sh
+++ examples/exp2.sh
@@ -5,6 +5,8 @@ val_size=64
 init_scale=1
 test_scale=0.2
 
+rm -r test_*
+
 for dist in books fb osm wiki; do
     echo "[${dist} 测试1] 只读吞吐率 | 99尾延迟 | zipf点查询平均延迟 | 随机加载构建时间"
     ./exp2 -p test_read_only -n ${init_scale} -m ${test_scale} -w rand -r zipf -d ${dist} -k ${key_size} -v ${val_size} -o ../results/${dist}_exp_read_only.txt
