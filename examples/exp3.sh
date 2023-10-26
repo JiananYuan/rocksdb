fd732df46a931ebc564e05b8ef5cb6df8beabc7c
JiananYuan <yuanjiajiaqq@163.com>
2023-10-26 22:07:39 +0800

    feat

--- examples/exp3.sh
+++ examples/exp3.sh
@@ -7,17 +7,16 @@ test_scale=10
 
 rm -rf test_*
 
-for dist in books fb osm wiki logn uniform; do
-  echo "[${dist} 测试1] 随机加载 | 吞吐率 | 99尾延迟 | zipf点查询平均延迟"
-  ./exp3 -p test_read_only_${dist} -n ${init_scale} -m ${test_scale} -w rand -d ${dist} -k ${key_size} -v ${val_size} -o ../results/${dist}_exp_read_only.txt -s 0 -f 0
-  rm -r test_read_only_${dist}
+# for dist in books fb osm wiki logn uniform; do
+#   echo "[${dist} 测试1] 随机加载 | 吞吐率 | 99尾延迟 | zipf点查询平均延迟"
+#   ./exp3 -p test_read_only_${dist} -n ${init_scale} -m ${test_scale} -w rand -d ${dist} -k ${key_size} -v ${val_size} -o ../results/${dist}_exp_read_only.txt -s 0 -f 0
+#   rm -r test_read_only_${dist}
 
-  echo "[${dist} 测试2] 顺序加载"
-  ./exp3 -p test_load_seq_${dist} -n ${init_scale} -m 0 -w seq -d ${dist} -k ${key_size} -v ${val_size} -o ../results/${dist}_exp_load_seq.txt
-  rm -r test_load_seq_${dist}
-done
+#   echo "[${dist} 测试2] 顺序加载"
+#   ./exp3 -p test_load_seq_${dist} -n ${init_scale} -m 0 -w seq -d ${dist} -k ${key_size} -v ${val_size} -o ../results/${dist}_exp_load_seq.txt
+#   rm -r test_load_seq_${dist}
+# done
 
-# for vs in 128 256 512 1024; do
 # for vs in 128 256 512 1024; do
 #   echo "[测试3 value_size: ${vs}] 不同value_size 对吞吐量的影响"
 #   ./exp3 -p test_value_size -n ${init_scale} -m ${test_scale} -w rand -d fb -k ${key_size} -v ${vs} -o ../results/fb_exp_value_size_${vs}.txt -s 0 -f 4
@@ -28,3 +27,15 @@ done
 #echo "[测试4] zipf点查询时延breakdown"
 #./exp3 -p test_read_breakdown -n ${init_scale} -m ${test_scale} -w rand -s 3 -e 4 -d books -k ${key_size} -v ${val_size} -o ../results/exp_read_breakdown.txt
 #rm -r test_read_breakdown
+
+for dist in books fb osm wiki logn uniform; do
+    echo "[测试4] ON DISK rand点查询时延breakdown"
+    ./exp3 -p test_read_breakdown_${dist} -n ${init_scale} -m ${test_scale} -w rand -l rand -s 3 -f 4 -d ${dist} -k ${key_size} -v ${val_size} -o ../results/exp_read_breakdown_${dist}_on_disk.txt
+    rm -r test_read_breakdown_${dist}
+done
+
+for dist in books fb osm wiki logn uniform; do
+    echo "[测试4] IN MEM rand点查询时延breakdown"
+    ./exp3 -p /mnt/rocksdb/${dist} -n ${init_scale} -m ${test_scale} -w rand -l rand -s 3 -f 4 -d ${dist} -k ${key_size} -v ${val_size} -o ../results/exp_read_breakdown_${dist}_in_mem.txt
+    rm -r /mnt/rocksdb/${dist}
+done
